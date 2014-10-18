10de2b17014ae7bcf3962b665ba5135ad045b4ca
Ryan Russell <russellsprouts2@gmail.com>
2014-10-17 20:42:31 -0700

    Events on the client

--- frontend/js/websockets.js
+++ frontend/js/websockets.js
@@ -1,13 +1,16 @@
 var Sockets = (function() {
   function connect() {
     var d = Q.defer()
-    var ws = new WebSocket("ws://localhost:1337/", 'rps');
-    ws.onopen = function() {
-      d.resolve(ws);
+    var ws = new WebSocket("ws://128.95.62.197:1337/", 'rps');
+    var emitter = new EventEmitter();
+
+    ws.onopen = function(a) {
+      d.resolve(new Connection(ws, emitter));
     };
 
     ws.onerror = function(err) {
       d.reject(err);
+      console.error(err);
     };
 
     return d.promise;
@@ -16,11 +19,37 @@ var Sockets = (function() {
   return {
     connect: connect
   };
+
+  function Connection(ws, ee) {
+    this.playerID = -1;
+    var self = this;
+    this.socket.onmessage = function(msg) {
+      var payload = JSON.parse(msg.data);
+      if(typeof payload.id == 'Number') {
+        self.playerID = payload.id;
+      }
+      this.emitter.emitEvent(payload.evt, payload.data);
+    };
+    this.socket = ws;
+    this.emitter = ee;
+  }
+
+  Connection.prototype.on = function(type, f) {
+    return this.emitter.addListener(type, f);
+  };
+
+  Connection.prototype.send(type, data) {
+    this.socket.send(JSON.stringify({
+      evt: type,
+      id: this.playerID,
+      d: data || null
+    }));
+  };
+
 })();
 
-Sockets.connect().then(function(socket) {
-  socket.send("HELLO WORLD -- from client");
-  socket.onmessage = function(a) {
-    console.log(a.data);
-  };
+
+
+Sockets.connect().then(function(connection) {
+  connection.send('join');
 }).catch(console.error);
