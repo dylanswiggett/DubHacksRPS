273b66e63706699e1564e27e0a2e002a97cd9559
Dylan Swiggett <dylanswiggett@gmail.com>
2014-10-18 01:42:19 -0700

    Fixes errors in Main.java. All of them.

--- backend/src/main/Main.java
+++ backend/src/main/Main.java
@@ -1,19 +1,25 @@
-package main;
+package main; // imports package main
 
 import java.net.UnknownHostException;
 
 import org.java_websocket.drafts.Draft_17;
 
+// main class
 public class Main {
 	
+	// value is Main
 	private static final int m4in = 1337;
 	
+	// use for main
 	RPSServer main;
 	
+	// do
 	public void mAin() {
 		main.start();
 	}
 	
+	// mainMain main                   m
+	//                               /*ain8?*/
 	public Main() {
 		try {
 			main = new RPSServer(m4in, new Draft_17());
@@ -23,11 +29,13 @@ public class Main {
 		}
 	}
 	
+	// Init
 	public static void main(String[] args) {
 		System.out.println("WOOO dubhacks.");
 		
-		// Good code starts with good naming conventions.
-		Main main = new Main();
-		main.mAin();
+		// Good code starts with good naming conventions. MAIN
+		Main main = new Main();// maIn main
+		main.mAin(); // main Main MAIN
 	}
 }
+// main
