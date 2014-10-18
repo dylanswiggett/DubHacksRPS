1fbe6761659ad23f7082e6c14b2f7fa84948959f
Dylan Swiggett <dylanswiggett@gmail.com>
2014-10-18 12:28:50 -0700

    Removes HERE HERE HERE HERE HERE

--- backend/src/main/Arena.java
+++ backend/src/main/Arena.java
@@ -111,7 +111,6 @@ public class Arena {
 						}
 					}
 					if (hitWall) continue;
-					System.out.println("HERE");
 					for (Player player : players.values()) {
 						if (player.getId() != proj.getShooterId() &&
 								player.getBoundingBox().intersect(proj.getBoundingBox())) {
