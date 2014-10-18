ba4988fe9f4e9b213acc12424aa8d12fe4b8d9ac
Jake Nash <jakenash23@gmail.com>
2014-10-18 04:43:21 -0700

    Make csv importer more general

--- backend/src/main/RuleImport.java
+++ backend/src/main/RuleImport.java
@@ -18,10 +18,23 @@ import org.json.JSONObject;
 public class RuleImport {
 
 	private static Map<String, Map<String, String>> ruleMap = null;
-	private static final String filePath = "../RPS.csv";
+	private static Map<String, Map<String, String>> metaMap = null;
+	private static final String ruleFilePath = "../RPS.csv";
+	private static final String metaFilePath = "../RPSMeta.csv";
 	
-	private static Map<String, Map<String, String>> importMap(boolean mirror) {
-		Path file = Paths.get(filePath);
+	private static Map<String, Map<String, String>> importMap(boolean basic) {
+		Path file;
+		if (basic) {
+			file = Paths.get(ruleFilePath);
+			return importRuleMap(file);
+		} else {
+			file = Paths.get(metaFilePath);
+			return importMetaMap(file);
+		}
+		
+	}
+	
+	private static Map<String, Map<String, String>> importRuleMap(Path file) {
 		
 		Charset charset = Charset.forName("US-ASCII");
 		try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
@@ -51,35 +64,76 @@ public class RuleImport {
 			System.err.format("IOException: %s%n", x);
 		}
 		
-		if (mirror) {
-			Set<String> keys = ruleMap.keySet();
-			for (String key : keys) {
-				Map<String, String> rowRuleMap = ruleMap.get(key);
-				Set<String> rowKeys = rowRuleMap.keySet();
-				for (String rowKey : rowKeys) {
-					String ruleValue = rowRuleMap.get(rowKey);
-					if (ruleValue.equals("")) {
-						Map<String, String> otherRowRuleMap = ruleMap.get(rowKey);
-						String otherRowRuleValue = otherRowRuleMap.get(key);
-						int otherRowRuleNum = Integer.parseInt(otherRowRuleValue);
-						otherRowRuleNum *= -1;
-						String newRuleValue = String.valueOf(otherRowRuleNum);
-						rowRuleMap.put(rowKey, newRuleValue);
-					}
+		Set<String> keys = ruleMap.keySet();
+		for (String key : keys) {
+			Map<String, String> rowRuleMap = ruleMap.get(key);
+			Set<String> rowKeys = rowRuleMap.keySet();
+			for (String rowKey : rowKeys) {
+				String ruleValue = rowRuleMap.get(rowKey);
+				if (ruleValue.equals("")) {
+					Map<String, String> otherRowRuleMap = ruleMap.get(rowKey);
+					String otherRowRuleValue = otherRowRuleMap.get(key);
+					int otherRowRuleNum = Integer.parseInt(otherRowRuleValue);
+					otherRowRuleNum *= -1;
+					String newRuleValue = String.valueOf(otherRowRuleNum);
+					rowRuleMap.put(rowKey, newRuleValue);
 				}
-				ruleMap.put(key, rowRuleMap);
 			}
+			ruleMap.put(key, rowRuleMap);
 		}
 		
 		return ruleMap;
 	}
 	
-	public static Map<String, Map<String, String>> getMap(boolean mirror) {
-		if (ruleMap == null) {
-			ruleMap = new HashMap<String, Map<String, String>>();
-			ruleMap = importMap(mirror);
+	private static Map<String, Map<String, String>> importMetaMap(Path file) {
+		Charset charset = Charset.forName("US-ASCII");
+		try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
+			String line = null;
+			List<String> paramList = null;
+			int numParams = 0;
+			line = reader.readLine();
+			if (line != null) {
+				paramList = Arrays.asList(line.split(","));
+				numParams = paramList.size();
+			}
+			while ((line = reader.readLine()) != null) {
+				List<String> row = Arrays.asList(line.split(",", numParams));
+				String key = null;
+				for (int i = 0; i < row.size(); i++) {
+					if (i == 0) {
+						key = row.get(i);
+						ruleMap.put(key, new HashMap<String, String>());
+					} else {
+						Map<String, String> rowRuleMap = ruleMap.get(key);
+						rowRuleMap.put(paramList.get(i), row.get(i));
+						ruleMap.put(key, rowRuleMap);
+					}
+				}
+			}
+		} catch (IOException x) {
+			System.err.format("IOException: %s%n", x);
 		}
-		return Collections.unmodifiableMap(ruleMap);
+		
+
+		return ruleMap;
+	}
+	
+	public static Map<String, Map<String, String>> getMap(boolean basic) {
+		if (basic && ruleMap == null) {
+			ruleMap = new HashMap<String, Map<String, String>>();
+			ruleMap = importMap(true);
+			return Collections.unmodifiableMap(ruleMap);
+		} else if (basic && ruleMap != null) {
+			return Collections.unmodifiableMap(ruleMap);
+		} else if (basic && metaMap == null) {
+			metaMap = new HashMap<String, Map<String, String>>();
+			metaMap = importMap(false);
+			return Collections.unmodifiableMap(metaMap);
+		} else {
+			return Collections.unmodifiableMap(metaMap);
+		}
+		
+		
 	}
 	
 	public static JSONObject toJSON() {
